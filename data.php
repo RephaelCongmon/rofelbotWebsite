fedb9194293b6f7cb8cf53b691db32cad301c748
rephcongmon <rephcongmon@gmail.com>
2019-03-07 22:10:41 -0500

    Added a lot of PHP

---
+++ data.php
@@ -0,0 +1,26 @@
+<?php
+    function debug_to_console( $data ) {
+        $output = $data;
+        if ( is_array( $output ) )
+            $output = implode( ',', $output);
+    
+        echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
+    }
+    
+    require_once 'dbconfig.php';
+
+    $dsn = "pgsql:host=$host;dbname=$db;user=$username;password=$password";
+
+    try{
+        $conn = new PDO($dsn);
+
+        if ($conn){
+            debug_to_console("Connected to the database successfully!")
+        }
+    }catch (PDOException $e){
+        echo $e->getMessage();
+    }
+
+    debug_to_console("Test");
+?>
+
---
+++ dbconfig.php
@@ -0,0 +1,8 @@
+<?php
+
+$host='ec2-107-22-162-8.compute-1.amazonaws.com;dbname=dabt8f8rtmeedv';
+$db = 'dabt8f8rtmeedv';
+$username = 'ptghoxkxwglhwh';
+$password = '735337cbbc02be13112d0a609e25abe4a3408d3f8ec9a966dcdbb5b0e746bc04'
+
+?>
