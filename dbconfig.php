e80513ea19d45e09874fe1eff224ac66bc09da4a
rephcongmon <rephcongmon@gmail.com>
2019-04-17 13:51:30 -0400

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,9 +1,6 @@
 <?php
 
-// $host='ec2-107-22-162-8.compute-1.amazonaws.com;dbname=dabt8f8rtmeedv';
-// $db = 'dabt8f8rtmeedv';
-// $username = 'ptghoxkxwglhwh';
-// $password = '735337cbbc02be13112d0a609e25abe4a3408d3f8ec9a966dcdbb5b0e746bc04'
+//I see you trying to hack into the mainframe (つ ͡° ͜ʖ ͡°)つ
 
 $host = getenv('dbHost');
 $db = getenv('db');
