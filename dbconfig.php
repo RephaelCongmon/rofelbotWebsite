3f22299f21d6fd9167be2f75d5607715cf5bb8ff
rephcongmon <rephcongmon@gmail.com>
2019-04-17 13:49:43 -0400

    stuff

--- dbconfig.php
+++ dbconfig.php
@@ -1,8 +1,13 @@
 <?php
 
-$host='ec2-107-22-162-8.compute-1.amazonaws.com;dbname=dabt8f8rtmeedv';
-$db = 'dabt8f8rtmeedv';
-$username = 'ptghoxkxwglhwh';
-$password = '735337cbbc02be13112d0a609e25abe4a3408d3f8ec9a966dcdbb5b0e746bc04'
+// $host='ec2-107-22-162-8.compute-1.amazonaws.com;dbname=dabt8f8rtmeedv';
+// $db = 'dabt8f8rtmeedv';
+// $username = 'ptghoxkxwglhwh';
+// $password = '735337cbbc02be13112d0a609e25abe4a3408d3f8ec9a966dcdbb5b0e746bc04'
+
+$host = getenv('dbHost');
+$db = getenv('db');
+$username = getenv('dbUserName');
+$password = getenv('dbPassword');
 
 ?>
